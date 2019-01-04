59f808166827df8284a4d67e2cbef80429cd188d
YC L <30719670+smallg0at@users.noreply.github.com>
2019-01-04 09:53:18 +0800

    Update a lot

--- Easier_Arduino/fastpin.h
+++ Easier_Arduino/fastpin.h
@@ -1,17 +1,18 @@
 #include "arduino.h"
-class port{
-	public:
-		byte port;
-		byte status;
-		void pmin();
-		void pmout();
-		void oti();
+#include <Servo.h>
+#include "Keyboard.h"
+Servo nine;
+Servo ten;
+void pmin(byte a[],int length){
+  for(int i=0;i<length;i++){
+	pinMode(a[i],0x00);
+  }
+  
 }
-void pmin(byte a){
-  pinMode(a,0x00);
-}
-void pmout(byte a){
-  pinMode(a,0x01);
+void pmout(byte a[],int length){
+  for(int i=0;i<length;i++){
+	pinMode(a[i],0x01);
+  }
 }
 void otiwd(byte a=9,byte b=A0,int delaytime=50,double times=1.0){//output to input with delay;
   if(delaytime>0){
@@ -20,7 +21,40 @@ void otiwd(byte a=9,byte b=A0,int delaytime=50,double times=1.0){//output to inp
 	int k2=analogRead(a);
 	analogWrite(b,((k1+k2)/2)*times);
   }else{
-	  int k=analogread(a);
+	  int k=analogRead(a);
 	  analogWrite(b,k*times);
+  }
+}
+void servoinit(bool nineenabled=false,bool tenenabled=false,bool mechanicinit=false,int mechanicdelay=1000){
+	if(nine)nine.attach(9);
+	if(ten)ten.attach(10);
+	if(mechanicinit){
+		if(nineenabled)nine.write(0);
+		if(tenenabled)ten.write(0);
+		delay(mechanicdelay);
+		if(nineenabled)nine.write(180);
+		if(tenenabled)ten.write(180);
+		delay(mechanicdelay);
+	}
+	
+}
+void servow(int nineang = 90,int tenang = 90){
+	if(nineang!=-1)nine.write(nineang);
+	if(tenang!=-1)ten.write(tenang);
+}
+void keypress(byte a[],int count=1,int holdtime=50,bool takeover=true){
+  if(takeover)Keyboard.begin();
+  for(int i=0;i<count;i++){
+    Keyboard.press(a[i]);
+  }
+  delay(holdtime);
+  
+  if(takeover){
+    Keyboard.releaseAll();
+    Keyboard.end();
+  }else{
+    for(int i=0;i<count;i++){
+    Keyboard.release(a[i]);
+    }
   }
 }
